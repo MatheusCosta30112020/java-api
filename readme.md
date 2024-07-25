c692a602918a607c0ea6173e1bb7d382b39775a5
Matheus <matheuscosta30112020@gmail.com>
2024-07-25 16:38:51 -0400

    api-pronta

--- readme.md
+++ readme.md
@@ -1,3 +1,7 @@
 #### APlicação java
 * mysql : porta : 3306
 * criar um banco : aula_spring_boot
+
+#### Entidades
+* [0] clients
+* [x] Aluno
--- src/main/java/com/treinamento/javaspring/model/Aluno.java
+++ src/main/java/com/treinamento/javaspring/model/Aluno.java
@@ -2,6 +2,7 @@ package com.treinamento.javaspring.model;
 
 import java.util.Date;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -19,12 +20,15 @@ public class Aluno {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true, nullable = false) // regras de proteção
     private String nome;
 
+    @Column(nullable = false)  // regras de proteção
     private String serie;
 
+    @Column(nullable = false)  // regras de proteção
     private String sexo;
 
-    @Temporal(TemporalType.DATE)
+    @Temporal(TemporalType.DATE)  // regras de proteção
     private Date dataCadastro = new Date();
 }
